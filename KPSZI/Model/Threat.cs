21a6862f3b38db9f73a2570dcd60c36f75908a5b
VizieR <zoom_18_@inbox.ru>
2018-03-19 16:03:46 +1000

    Мои изменения

--- KPSZI/Model/Threat.cs
+++ KPSZI/Model/Threat.cs
@@ -6,12 +6,46 @@ using System.Threading.Tasks;
 
 namespace KPSZI.Model
 {
+    /// <summary>
+    /// Класс-сущность угроза
+    /// </summary>
     public class Threat
     {
-        //Класс-сущность для угрозы
+        /// <summary>
+        /// ID угрозы
+        /// </summary>
         public int ThreatId { get; set; }
+        /// <summary>
+        /// Наименование УБИ
+        /// </summary>
         public string Name { get; set; }
+        /// <summary>
+        /// Описание УБИ
+        /// </summary>
         public string Description { get; set; }
-        public string ThreatSource { get; set; }
+        /// <summary>
+        /// Объект воздействия
+        /// </summary>
+        public string ObjectOfInfluence { get; set; }
+        /// <summary>
+        /// Нарушение конфиденциальности
+        /// </summary>
+        public bool ConfidenceViolation { get; set; }
+        /// <summary>
+        /// Нарушение целостности
+        /// </summary>
+        public bool IntegrityViolation { get; set; }
+        /// <summary>
+        /// Нарушение доступности
+        /// </summary>
+        public bool AvailabilityViolation { get; set; }
+        /// <summary>
+        /// Дата добавления угрозы в БДУ
+        /// </summary>
+        public DateTime DateOfAdd { get; set; }
+        /// <summary>
+        /// Последняя дата изменения угрозы
+        /// </summary>
+        public DateTime DateOfChange { get; set; }
     }
 }
---
+++ KPSZI/Model/ThreatSource.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KPSZI.Model
+{
+    /// <summary>
+    /// Класс-сущность для справочника 
+    /// </summary>
+    public class ThreatSource
+    {
+        /// <summary>
+        /// Id для записи
+        /// </summary>
+        public int ThreatSourceId { get; set; }
+        /// <summary>
+        /// Внешняя ссылка на Id угрозы
+        /// </summary>
+        public int ThreatId { get; set; }
+        /// <summary>
+        /// Внутренний ли нарушитель? true - да, false - нет.
+        /// </summary>
+        public bool InternalIntruder { get; set; }
+        /// <summary>
+        /// Потенциал нарушителя. 0 - низкий, 1 - средний, 2 - высокий.
+        /// </summary>
+        public int Potencial { get; set; }
+
+    }
+}
