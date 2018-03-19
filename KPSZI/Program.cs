54e0c48016858fe0ec6544389ea2e09f2b56b240
ooVizieRoo <zoom_18_@inbox.ru>
2018-03-19 18:42:25 +1000

    Fix two

--- KPSZI/Program.cs
+++ KPSZI/Program.cs
@@ -17,7 +17,6 @@ namespace KPSZI
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
-            //ZALUPA
         }
     }
 }
