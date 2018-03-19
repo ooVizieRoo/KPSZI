3e0dab305769b6e0aba1b6f2a65bc11dd84d85a5
ooVizieRoo <zoom_18_@inbox.ru>
2018-03-19 18:51:22 +1000

    Test on conflicts

--- KPSZI/Program.cs
+++ KPSZI/Program.cs
@@ -17,6 +17,8 @@ namespace KPSZI
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            //lalalala
+            //VizieR is the best nickname!
         }
     }
 }
