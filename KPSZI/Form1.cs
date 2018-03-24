42280613f782b94c5b35c23f5ed3edd3db01d460
Nakonechnyi <sanya-1995-14@mail.ru>
2018-03-24 15:35:15 +1000

    Изменил код в форме

--- KPSZI/Form1.cs
+++ KPSZI/Form1.cs
@@ -19,19 +19,19 @@ namespace KPSZI
 
             using (KPSZIContext db = new KPSZIContext())
             {
-                Threat thr1 = new Threat();
-                thr1.ThreatNumber = 1;
-                thr1.Name = "Threat1";
-                thr1.Description = "Description1";
-                thr1.AvailabilityViolation = false;
-                thr1.IntegrityViolation = false;
-                thr1.ConfidenceViolation = true;
-                thr1.DateOfAdd = DateTime.Today;
-                thr1.DateOfChange = DateTime.Now;
-                thr1.ObjectOfInfluence = "PC";
+                //Threat thr1 = new Threat();
+                //thr1.ThreatNumber = 1;
+                //thr1.Name = "Threat1";
+                //thr1.Description = "Description1";
+                //thr1.AvailabilityViolation = false;
+                //thr1.IntegrityViolation = false;
+                //thr1.ConfidenceViolation = true;
+                //thr1.DateOfAdd = DateTime.Today;
+                //thr1.DateOfChange = DateTime.Now;
+                //thr1.ObjectOfInfluence = "PC";
 
-                db.Threats.Add(thr1);
-                db.SaveChanges();
+                //db.Threats.Add(thr1);
+                //db.SaveChanges();
 
                 foreach (Threat thr in db.Threats)
                 {
