987133648983bccb97f54e95923894733600dff3
ooVizieRoo <vizier.khv@gmail.com>
2018-03-25 18:23:14 +1000

    no comment

--- KPSZI/Form1.cs
+++ KPSZI/Form1.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,28 +18,19 @@ namespace KPSZI
         {
             InitializeComponent();
 
-            using (KPSZIContext db = new KPSZIContext())
-            {
-                Threat thr1 = new Threat();
-                thr1.ThreatNumber = 1;
-                thr1.Name = "Threat1";
-                thr1.Description = "Description1";
-                thr1.AvailabilityViolation = false;
-                thr1.IntegrityViolation = false;
-                thr1.ConfidenceViolation = true;
-                thr1.DateOfAdd = DateTime.Today;
-                thr1.DateOfChange = DateTime.Now;
-                thr1.ObjectOfInfluence = "PC";
-
-                db.Threats.Add(thr1);
-                db.SaveChanges();
-
-                foreach (Threat thr in db.Threats)
-                {
-                    Console.WriteLine("{0}:{1}:{2}:{3}", thr.ThreatId, thr.ThreatNumber, thr.Name, thr.Description);
-                }
-            }
+            //using (KPSZIContext db = new KPSZIContext())
+            //{
+            //    foreach (Threat thr in db.Threats)
+            //    {
+            //        Console.WriteLine("{0}:{1}:{2}:{3}", thr.ThreatId, thr.ThreatNumber, thr.Name, thr.Description);
+            //    }
+            //}
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FileInfo fi = new FileInfo("thrlist.xlsx");
+            Threat.GetThreatsFromXlsx(fi);
+        }
     }
 }
--- KPSZI/Model/Threat.cs
+++ KPSZI/Model/Threat.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -51,5 +53,96 @@ namespace KPSZI.Model
         /// Последняя дата изменения угрозы
         /// </summary>
         public DateTime DateOfChange { get; set; }
+
+        public static Threat[] GetThreatsFromXlsx(FileInfo file)
+        {
+            List<List<string>> listOfXlslxRows = File_Selected_New(file);
+
+            Threat[] listOfAllThreatsFromFile = new Threat[listOfXlslxRows.Count];
+            for (int i = 0; i < listOfAllThreatsFromFile.Length - 1; i++)
+            {
+                listOfAllThreatsFromFile[i].ThreatNumber = listOfXlslxRows[1][i];
+            }
+
+            return null;
+        }
+
+        private static string GetConnectionString(FileInfo _file)
+        {
+            Dictionary<string, string> props = new Dictionary<string, string>();
+
+
+            // XLSX - Excel 2007, 2010, 2012, 2013
+            if (_file.Extension == ".xlsx")
+            {
+                props["Provider"] = "Microsoft.ACE.OLEDB.12.0;";
+                props["Extended Properties"] = "Excel 12.0 XML";
+                props["Data Source"] = _file.FullName;
+            }
+            else if (_file.Extension == ".xls")
+            {
+                props["Provider"] = "Microsoft.Jet.OLEDB.4.0";
+                props["Extended Properties"] = "Excel 8.0";
+                props["Data Source"] = "C:\\MyExcel.xls";
+            }
+            else throw new Exception("Неизвестное расширение файла!");
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (KeyValuePair<string, string> prop in props)
+            {
+                sb.Append(prop.Key);
+                sb.Append('=');
+                sb.Append(prop.Value);
+                sb.Append(';');
+            }
+
+            return sb.ToString();
+        }
+
+        private static List<List<string>> File_Selected_New(FileInfo _file)
+        {
+            DataSet ds = new DataSet();
+            string connectionString = GetConnectionString(_file);
+
+            using (System.Data.OleDb.OleDbConnection conn = new System.Data.OleDb.OleDbConnection(connectionString))
+            {
+                conn.Open();
+                System.Data.OleDb.OleDbCommand cmd = new System.Data.OleDb.OleDbCommand();
+                cmd.Connection = conn;
+
+                // Get all Sheets in Excel File
+                DataTable dtSheet = conn.GetOleDbSchemaTable(System.Data.OleDb.OleDbSchemaGuid.Tables, null);
+
+                // Loop through all Sheets to get data
+                foreach (DataRow dr in dtSheet.Rows)
+                {
+                    string sheetName = dr["TABLE_NAME"].ToString();
+
+                    // Get all rows from the Sheet
+                    cmd.CommandText = "SELECT * FROM [" + sheetName + "]";
+
+                    DataTable dt = new DataTable();
+                    dt.TableName = sheetName;
+
+                    System.Data.OleDb.OleDbDataAdapter da = new System.Data.OleDb.OleDbDataAdapter(cmd);
+                    da.Fill(dt);
+
+                    ds.Tables.Add(dt);
+                }
+                
+                // В list_table хранится информация по строкам, начиная с [0]
+                List<List<string>> list_table = new List<List<string>>();
+                for (int j = 0; j < ds.Tables[0].Columns.Count; j++)
+                {
+                    List<string> list_row = new List<string>();
+                    for (int i = 1; i < ds.Tables[0].Rows.Count; i++)
+                        list_row.Add(ds.Tables[0].Rows[i].ItemArray[j].ToString());
+                    list_table.Add(list_row);
+                }
+
+                return list_table;
+            }
+        }
     }
 }
