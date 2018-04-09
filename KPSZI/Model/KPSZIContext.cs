c10dbd8df6812743002e22225c19ddf9d9a6949a
ooVizieRoo <vizier.khv@gmail.com>
2018-04-09 15:38:07 +1000

    Поправлена инициализация БД

--- KPSZI/Model/KPSZIContext.cs
+++ KPSZI/Model/KPSZIContext.cs
@@ -221,12 +221,14 @@ namespace KPSZI.Model
             #region Структурно-функциональные характеристики 
             SFHType sfh1 = new SFHType();
             sfh1.Name = "По структуре ИС";
+            sfh1.MultipleChoice = false;
             sfh1.SFHs.Add(new SFH { Name = "Автономное автоматизированное рабочее место", ProjectSecurity = 2 });
             sfh1.SFHs.Add(new SFH { Name = "Локальная ИС", ProjectSecurity = 1 });
             sfh1.SFHs.Add(new SFH { Name = "Распределенная ИС", ProjectSecurity = 0 });
 
             SFHType sfh2 = new SFHType();
             sfh2.Name = "По используемым информационным технологиям";
+            sfh2.MultipleChoice = true;
             sfh2.SFHs.Add(new SFH { Name = "Системы на основе виртуализации", ProjectSecurity = 0 });
             sfh2.SFHs.Add(new SFH { Name = "Системы, реализущие \"облачные вычисления\"", ProjectSecurity = 0 });
             sfh2.SFHs.Add(new SFH { Name = "Системы с мобильными устройствами", ProjectSecurity = 0 });
@@ -236,6 +238,7 @@ namespace KPSZI.Model
 
             SFHType sfh3 = new SFHType();
             sfh3.Name = "По архитектуре информационной системы";
+            sfh3.MultipleChoice = true;
             sfh3.SFHs.Add(new SFH { Name = "Системы на основе \"тонкого клиента\"", ProjectSecurity = 2 });
             sfh3.SFHs.Add(new SFH { Name = "Системы на основе одноранговой сети", ProjectSecurity = 1 });
             sfh3.SFHs.Add(new SFH { Name = "Файл-серверные системы", ProjectSecurity = 0 });
@@ -247,33 +250,39 @@ namespace KPSZI.Model
 
             SFHType sfh4 = new SFHType();
             sfh4.Name = "По наличию (отсутствию) взаимосвязей с иными информационными системами";
+            sfh4.MultipleChoice = false;
             sfh4.SFHs.Add(new SFH { Name = "Взаимодействующая с системами", ProjectSecurity = 0 });
             sfh4.SFHs.Add(new SFH { Name = "Невзаимодействующая с системами", ProjectSecurity = 1 });
 
             SFHType sfh5 = new SFHType();
             sfh5.Name = "По наличию (отсутствию) взаимосвязей (подключений)";
+            sfh5.MultipleChoice = false;
             sfh5.SFHs.Add(new SFH { Name = "Подключенная к сетям связи общего пользования", ProjectSecurity = 0 });
             sfh5.SFHs.Add(new SFH { Name = "Подключенная через выделенную инфрастркутуру (gov.ru ил иную)", ProjectSecurity = 1 });
             sfh5.SFHs.Add(new SFH { Name = "Неподключенная", ProjectSecurity = 2 });
 
             SFHType sfh6 = new SFHType();
             sfh6.Name = "По размещению технических средств";
+            sfh6.MultipleChoice = false;
             sfh6.SFHs.Add(new SFH { Name = "Расположенные в пределах одной контролируемой зоны", ProjectSecurity = 2 });
             sfh6.SFHs.Add(new SFH { Name = "Расположенные в пределах нескольких контролируемых зон", ProjectSecurity = 1 });
             sfh6.SFHs.Add(new SFH { Name = "Расположенные вне контролируемой зоны", ProjectSecurity = 0 });
 
             SFHType sfh7 = new SFHType();
             sfh7.Name = "По режимам обработки информации в ИС";
+            sfh7.MultipleChoice = false;
             sfh7.SFHs.Add(new SFH { Name = "Многопользовательский", ProjectSecurity = 0 });
             sfh7.SFHs.Add(new SFH { Name = "Однопользовательский", ProjectSecurity = 2 });
 
             SFHType sfh8 = new SFHType();
             sfh8.Name = "По режимам разграничения прав доступа";
+            sfh8.MultipleChoice = false;
             sfh8.SFHs.Add(new SFH { Name = "Без разграничения", ProjectSecurity = 0 });
             sfh8.SFHs.Add(new SFH { Name = "С разграничением", ProjectSecurity = 1 });
 
             SFHType sfh9 = new SFHType();
             sfh9.Name = "По режимам разделения функций по управлению информационной системой";
+            sfh9.MultipleChoice = true;
             sfh9.SFHs.Add(new SFH { Name = "Без разделения", ProjectSecurity = 0 });
             sfh9.SFHs.Add(new SFH { Name = "Выделение рабочих мест для администрирования в отдельный домен", ProjectSecurity = 1 });
             sfh9.SFHs.Add(new SFH { Name = "Использование различных сетевых адресов", ProjectSecurity = 1 });
@@ -281,6 +290,7 @@ namespace KPSZI.Model
 
             SFHType sfh10 = new SFHType();
             sfh10.Name = "По подходам к сегментированию";
+            sfh9.MultipleChoice = false;
             sfh10.SFHs.Add(new SFH { Name = "Без сегментирования", ProjectSecurity = 0});
             sfh10.SFHs.Add(new SFH { Name = "С сегментированием", ProjectSecurity = 1 });
 
