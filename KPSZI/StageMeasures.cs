11e31872e162f4cd40bc483a81fa127036d4c53a
ooVizieRoo <vizier.khv@gmail.com>
2018-06-11 14:02:36 +1000

    Создал вкладку Measures

---
+++ KPSZI/StageMeasures.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KPSZI
+{
+    class StageMeasures:Stage
+    {
+        protected override ImageList imageListForTabPage { get; set; }
+
+        StageMeasures(TabPage stageTab, TreeNode stageNode, MainForm mainForm, InformationSystem IS)
+            : base(stageTab, stageNode, mainForm, IS)
+        {
+
+        }
+
+        protected override void initTabPage()
+        {
+
+        }
+
+        public override void saveChanges()
+        {
+
+        }
+
+        public override void enterTabPage()
+        {
+
+        }
+
+
+    }
+}
